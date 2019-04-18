0252e6f8cdd689d4d6f5af795ab8d42bbf87c609
Logan Hufstetler <logan.hufstetler@gmail.com>
2019-04-18 11:57:59 -0600

    started testing db functions

---
+++ models/parents.spec.js
@@ -0,0 +1,12 @@
+const db = require('../database/dbConfig.js');
+const Parents = require('./parents.js');
+
+describe("parents.js", () => {
+  describe("add()", () => {
+    beforeEach(async () => {
+      await db("parents").truncate();
+    });
+
+    await 
+  });
+});
